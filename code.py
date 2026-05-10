e586ce20483ca51927f15a0531e38b7521d68dcd
= <mahimsharma.official@gmail.com>
2026-05-10 13:58:26 +0530

    basic numpy arrays

--- code.py
+++ code.py
@@ -1,5 +1,38 @@
 import numpy as np
 
-arr = np.array([1, 2, 3])
-print(arr * 10)
-print(np.__version__)
+# python list 
+py_list =[1,2,3,4,5,6]
+
+# print(py_list *2)
+
+# numpy array 
+
+arr =np.array([1,2,3])
+
+# print(arr *2)
+
+
+# print(type(arr))
+# print(type(py_list))
+
+zero_dimension_array =np.array("A")
+
+
+# prints the dimension of the array (.ndim)
+# print(zero_dimension_array.ndim)
+
+two_dimension_array =np.array([[1,2,3],[4,5,6]])
+
+# prints the dimension of the array (.ndim)
+# print(two_dimension_array.ndim)
+
+three_dimension_array =np.array([[[1,2,3],[4,5,6],[7,8,9]],[[1,2,3],[4,5,6],[7,8,9]], [[1,2,3],[4,5,6],[7,8,9]]])
+
+# prints the dimension of the array (.ndim)
+# print(three_dimension_array.ndim)
+
+# prints the shape tuple of an array
+# print(three_dimension_array.shape)
+
+
+
